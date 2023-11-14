877f55606ba40a6ba3bfc7edb50aa521df537c61
manuersito <manuelbc03@hotmail.com>
2023-11-14 18:02:52 +0100

    seguir

--- Programacion/Tema_5/src/Tema_5_Propuestas/Menu_tablas.java
+++ Programacion/Tema_5/src/Tema_5_Propuestas/Menu_tablas.java
@@ -4,170 +4,156 @@ import java.util.*;
 
 public class Menu_tablas {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int op, nElemento = 0;
-		int[] vector = new int[10];
-		for (int i = 0; i < vector.length; i++) {
-			vector[i]=1;
-		}
-		
-		
-		
-		
-		do {
-			menu();
-			op = opcion();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int op, nElemento = 0;
+        int[] vector = new int[10];
 
-			switch (op) {
-			case 1:
-				menuInsertar(vector,nElemento);
-				opcion();
+        do {
+            menu();
+            op = opcion();
 
-				break;
-			case 2:
-				menuEliminar();
-				opcion();
-				break;
+            switch (op) {
+                case 1:
+                    nElemento = menuInsertar(vector, nElemento);
+                    break;
 
-			case 3:
+                case 2:
+                   nElemento = menuEliminar(vector, nElemento);
+                    break;
 
-				break;
-			case 4:
+                case 3:
+                    // Agrega la lógica para la opción 3
+                    break;
 
-				break;
-			case 5:
+                case 4:
+                   listar(vector);
+                    break;
 
-				break;
-			case 6:
+                case 5:
+                    // Agrega la lógica para la opción 5
+                    break;
 
-				break;
+                case 6:
+                    // Agrega la lógica para la opción 6
+                    break;
+            }
 
-			default:
-				break;
-			}
-			
-		} while (op!=7);
-		
-	}
+        } while (op != 7);
+    }
 
-	public static int opcion() {
-		Scanner sc = new Scanner(System.in);
-		int op = sc.nextInt();
-		return op;
-	}
+    public static int opcion() {
+        Scanner sc = new Scanner(System.in);
+        int op = sc.nextInt();
+        return op;
+    }
 
-	public static void menu() {
+    public static void menu() {
+        System.out.println(
+                "Elige opcion: \n 1.Insertar \n 2.Eliminar \n 3.Modificar \n 4.Listar \n 5.Ordenar \n 6.Buscar \n 7.Salir");
+    }
 
-		System.out.println(
-				"Elige opcion: \n 1.Insertar \n 2.Eliminar \n 3.Modificar \n 4.Listar \n 5.Ordenar \n 6.Buscar \n 7.Salir");
+    public static int menuInsertar(int vector[], int nElemento) {
+        int op;
 
-	}
+        System.out.println(
+                "Elige opcion: \n 1.Insertar por principio \n 2.Insertar por final \n 3.Insertar en lugar concreto \n 4.Salir");
 
-	
-	
-	
-	
-	
-	public static void menuInsertar(int vector[],int nElemento) {
-		int op;
+        op = opcion();
 
-		System.out.println(
-				"Elige opcion: \n 1.Insertar por principio \n 2.Insertar por final \n 3.Insertar en lugar concreto \n 4.Salir");
+        switch (op) {
+            case 1:
+                nElemento = insertarP(vector, nElemento);
+                break;
 
-		op = opcion();
+            case 2:
+                nElemento = insertarF(vector, nElemento);
+                break;
 
-		switch (op) {
-		case 1:
-			insertarP(vector, nElemento);
-			break;
-		case 2:
+            case 3:
+                // Agrega la lógica para la opción 3
+                break;
+        }
 
-			break;
-		case 3:
+        return nElemento;
+    }
 
-			break;
+    
 
-		default:
-			break;
-		}
-	
+	public static int menuEliminar(int[] vector, int nElemento) {
+        int op;
 
-	}
+        System.out.println(
+                "Elige opcion: \n 1.Eliminar por principio \n 2.Eliminar por final \n 3.Eliminar en lugar concreto \n 4.Salir");
 
-	
-	
-	
-	
-	public static void menuEliminar() {
-		int op;
+        op = opcion();
 
-		System.out.println(
-				"Elige opcion: \n 1.Eliminar por principio \n 2.Eliminar por final \n 3.Eliminar en lugar concreto \n 4.Salir");
+        switch (op) {
+            case 1:
+                // Agrega la lógica para la opción 1
+                break;
 
-		op = opcion();
-		
-		switch (op) {
-		case 1:
+            case 2:
+                // Agrega la lógica para la opción 2
+                break;
 
-			break;
-		case 2:
-
-			break;
-		case 3:
-
-			break;
-
-		default:
-			break;
-		}
-
-	}
-	
-	
-	
-	
-	
-	
-	private static int insertarP(int [] vector, int nElemento) {
-		
-		int num;
-		
-		if (nElemento==vector.length-1) {
-			System.out.println("Error");
-		} else {
-			System.out.println("Que numero quieres meter");
-			num=opcion();
-			int [] auxiliar = new int [vector.length+1];
-			auxiliar[0]=num;
-			 
-			for (int i = 0; i < vector.length; i++) {
-			    auxiliar[i+1] = vector[i];
-			}
-			 
-			vector = auxiliar;
-			nElemento++;
-			for (int i = 0; i < vector.length; i++) {
-				System.out.print(vector[i]+", ");
-				
-			}
-		}
-		
-		
+            case 3:
+                // Agrega la lógica para la opción 3
+                break;
+        }
 		return nElemento;
+    }
+
 	
-		
+	// metodo insertar principio
+	// pasamos el vector y el numero de elemntos que hay en el array
+    private static int insertarP(int[] vector, int nElemento) {
+        int num;
+// este if mira si hay 1 hueco en el array si lo hay procede a meterlo y si no da error
+        if (nElemento == vector.length - 1) {
+            System.out.println("Error");
+        } else {
+            System.out.println("Que numero quieres meter");
+            num = opcion();
+// este for mueve todos los elemento 1 posicion a la derecha para poder insertar por el principio
+            for (int i = nElemento; i > 0; i--) {
+                vector[i] = vector[i - 1];
+            }
+// aqui metemos en la posicion 0 del array el numero elejido y sumamos 1 al contador de elementos
+            vector[0] = num;
+            nElemento++;
+
+        }
+
+        return nElemento;
+    }
+    
+    
+    
+    
+    private static int insertarF(int[] vector, int nElemento) {
+    	 if (nElemento == vector.length - 1) {
+             System.out.println("Error");
+         } else {
+             System.out.println("Que numero quieres meter");
+             int num = opcion();
+
+             
+ // aqui metemos en la posicion 9 del array el numero elejido y sumamos 1 al contador de elementos
+             vector[9] = num;
+             nElemento++;
+
+         }
+
+		return nElemento;
 	}
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
+    
+    public static void listar(int[] vector) {
+		for (int i = 0; i < vector.length; i++) {
+			System.out.print(vector[i]+", ");
+		}
+		System.out.println();
+	}
+    
+    
 }
