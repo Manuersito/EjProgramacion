8d66237de4ab8f91f9bc20a74c931ac213f41670
manuersito <manuelbc03@hotmail.com>
2023-11-14 09:24:33 +0100

    para clase

--- Programacion/Tema_5/src/Tema_5_Propuestas/Menu_tablas.java
+++ Programacion/Tema_5/src/Tema_5_Propuestas/Menu_tablas.java
@@ -1,5 +1,7 @@
 package Tema_5_Propuestas;
+
 import java.util.*;
+
 public class Menu_tablas {
 
 	public static void main(String[] args) {
@@ -7,13 +9,17 @@ public class Menu_tablas {
 		Scanner sc = new Scanner(System.in);
 		int op;
 		do {
-			System.out.println("Elige opcion: \n 1.Insertar \n 2.Eliminar \n 3.Modificar \n 4.Listar \n 5.Ordenar \n 6.Buscar \n 7.Salir");
-			 op = sc.nextInt();
-			
-		} while (op!=7);
+			System.out.println(
+					"Elige opcion: \n 1.Insertar \n 2.Eliminar \n 3.Modificar \n 4.Listar \n 5.Ordenar \n 6.Buscar \n 7.Salir");
+			op = sc.nextInt();
+
+		} while (op < 7);
 		switch (op) {
 		case 1:
-
+			do {
+System.out.println("Elige opcion: \n 1.Insertar por principio \n 2.Insertar por final \n 3.Insertar en lugar concreto \n 4.Salir");
+op =sc.nextInt();
+			} while (op< 4);
 			break;
 		case 2:
 
