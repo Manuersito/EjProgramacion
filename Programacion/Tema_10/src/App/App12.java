3b45c255d667522b6d9004211af6a478236adc70
Manuersito <Manuelbc03@hotmail.com>
2024-03-13 11:09:12 +0100

    terminar

---
+++ Programacion/Tema_10/src/App/App12.java
@@ -0,0 +1,14 @@
+package App;
+
+public class App12 {
+
+	public static void main(String[] args) {
+		
+		double peso,altura, mediaPeso, mediaAltura,edad, mediaEdad;
+		int cont=0;
+		
+		
+
+	}
+
+}
---
+++ Programacion/Tema_10/src/App/App8.java
@@ -0,0 +1,16 @@
+package App;
+
+public class App8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	 
+	Integer[] leerEnteros(String texto) {
+		
+		
+		return null;
+		
+	}
+}
