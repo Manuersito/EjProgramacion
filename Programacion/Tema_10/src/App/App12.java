1170b6786d92a852a159a6938e637405c22cb82c
Manuersito <Manuelbc03@hotmail.com>
2024-03-14 12:44:10 +0100

    terminar

--- Programacion/Tema_10/src/App/App12.java
+++ Programacion/Tema_10/src/App/App12.java
@@ -1,12 +1,31 @@
 package App;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Scanner;
+
 public class App12 {
 
 	public static void main(String[] args) {
 		
 		double peso,altura, mediaPeso, mediaAltura,edad, mediaEdad;
 		int cont=0;
-		
+		FileInputStream in;
+		try {
+			in = new FileInputStream("deportistas.txt");
+			Scanner sc = new Scanner(in);
+			while (sc.hasNext()) {
+				String nombre = sc.next();
+				while (!sc.hasNextInt()) {
+					
+				}
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		
 
 	}
